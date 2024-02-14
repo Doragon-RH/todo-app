b336e1bd9c2234f1ddf2dec8503192d034bc1326
Doragon-RH <r216af@icloud.com>
2024-02-15 00:04:58 +0900

    タイトルの変更

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,6 +1,26 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
+import { object, string, number, date, InferType } from 'yup';
 
+let userSchema = object({
+  name: string().required(),
+  age: number().required().positive().integer(),
+  email: string().email(),
+  website: string().url().nullable(),
+  createdOn: date().default(() => new Date()),
+});
+
+// parse and assert validity
+const user = await userSchema.validate(await fetchUser());
+
+type User = InferType<typeof userSchema>;
+/* {
+  name: string;
+  age: number;
+  email?: string | undefined
+  website?: string | null | undefined
+  createdOn: Date
+}*/
 type Data = {
   name: string;
 };
